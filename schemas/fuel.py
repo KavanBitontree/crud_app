83b5945d0cd4c9fdefe8af7de084de3cc5521d8e
kavangajera <145152936+kavangajera@users.noreply.github.com>
2025-12-12 15:31:56 +0530

    removed date field from fuel entry form | user won't enter the date

--- schemas/fuel.py
+++ schemas/fuel.py
@@ -7,7 +7,6 @@ class FuelEntryBase(BaseModel):
     car_id: int = Field(..., description="ID of the car associated with this fuel entry")
     liters: float = Field(..., gt=0, description="Amount of fuel added in liters")
     kilometers: float = Field(..., gt=0, description="Distance driven since last refill in kilometers")
-    # date_of_fuel_entry:datetime  = Field(..., description="Date of the fuel entry")
 
 class FuelEntryCreate(FuelEntryBase):
     pass
@@ -15,7 +14,6 @@ class FuelEntryCreate(FuelEntryBase):
 class FuelEntryUpdate(BaseModel):
     liters: float | None = Field(None, gt=0, description="Amount of fuel added in liters")
     kilometers: float | None = Field(None, gt=0, description="Distance driven since last refill in kilometers")
-    # date_of_fuel_entry: datetime | None = Field(None, description="Date of the fuel entry")
 
 class FuelEntryResponse(FuelEntryBase):
     id: int
